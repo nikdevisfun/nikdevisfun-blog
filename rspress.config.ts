0f6c167a2ccefce7fb66176c796195acc9b0378f
nik26788 <risinnshow@gmail.com>
2026-01-22 10:20:01 +0800

    init nikdevisfun blog

---
+++ rspress.config.ts
@@ -0,0 +1,28 @@
+import * as path from "node:path";
+import { defineConfig } from "rspress/config";
+
+export default defineConfig({
+    root: path.join(__dirname, "docs"),
+    base: "/blog",
+    title: "NikDevIsFun",
+    description: "tech & life",
+    icon: "/rspress-icon.png",
+    logo: {
+        light: "/rspress-light-logo.png",
+        dark: "/rspress-dark-logo.png",
+    },
+    themeConfig: {
+        nav: [
+            { text: "Blog", link: "/" },
+            { text: "Github", link: "http://github.com/nikdevisfun" },
+        ],
+
+        socialLinks: [
+            {
+                icon: "github",
+                mode: "link",
+                content: "https://github.com/web-infra-dev/rspress",
+            },
+        ],
+    },
+});
